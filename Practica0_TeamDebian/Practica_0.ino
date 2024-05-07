188a29a7f5dd671a0c0483bd71c4bc2d42319668
SANTY FRANCISCO MARTINEZ CASTELLANOS <158227857+SantyDebian19@users.noreply.github.com>
2024-05-07 00:05:00 -0700

    Practica 0
    Escribir Hola Mundo en Raspberry Pi W desde Arduino IDE

---
+++ Practica0_TeamDebian/Practica_0.ino
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+const int ledPin = 25; // Pin GPIO donde está conectado el LED
+
+void setup() {
+    // Configurar el pin del LED como salida
+    pinMode(ledPin, OUTPUT);
+    
+    // Iniciar la comunicación serial a 115200 baudios
+    Serial.begin(115200);
+    
+    // Mensaje de bienvenida
+    Serial.println("Hola, Raspberry Pi Pico W!");
+}
+
+void loop() {
+     
+    // Apagar el LED
+    digitalWrite(ledPin, LOW);
+    Serial.println("Hello World");
+    
+    // Esperar 1 segundo
+    delay(10000);
+}
