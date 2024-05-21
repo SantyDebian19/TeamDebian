586aac8b1e9e24e0b661b1d3331656c527bc7427
SANTY FRANCISCO MARTINEZ CASTELLANOS <158227857+SantyDebian19@users.noreply.github.com>
2024-05-20 21:18:53 -0700

    Practica2_TeamDebian.ino

--- Practica2_TeamDebian/Practica2_TeamDebian.ino
+++ Practica2_TeamDebian/Practica2_TeamDebian.ino
@@ -4,16 +4,22 @@
 //Santy Francisco Martinez Castellanos - 21211989
 //Julio Alejandro Hernández León -21211963
 //Luis Roberto Leal Lua - 21211970
+
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 Adafruit_SSD1306 display(128, 64);
 
 void setup() { 
-  // Initialize the display
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // Address 0x3C for 128x32
-  // Clear the display
+  // Inicializa el display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // Direccion 0x3C para 128x32
+  
+  // Limpia el display
   display.clearDisplay();
+  
+  //Se asigna el color del texto
   display.setTextColor(WHITE);
+
+  //Se asignan valores de letra y el texto a mostrar
   display.setTextSize(2);
   display.setCursor(0, 0);
   display.println("Team Debian");
